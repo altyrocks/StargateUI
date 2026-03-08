1af0d1cf0c6c4282969ae71df250967043205b66
Scott Althaus <scott.althaus@gmail.com>
2026-03-07 22:49:48 -0600

    Modified for Azure Deployment

--- Program.cs
+++ Program.cs
@@ -16,21 +16,45 @@ builder.Services.AddCors(options =>
     });
 });
 
-builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<ILogService, LogService>();
-builder.Services.AddDbContext<StargateContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("StargateDb")));
-builder.Services.AddMediatR(cfg => {cfg.RegisterServicesFromAssemblies(typeof(Program).Assembly);});
+
+builder.Services.AddDbContext<StargateContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("StargateDb"),
+        sqlOptions =>
+        {
+            sqlOptions.EnableRetryOnFailure(
+                maxRetryCount: 5,
+                maxRetryDelay: TimeSpan.FromSeconds(10),
+                errorNumbersToAdd: null);
+        })
+        .EnableDetailedErrors()
+);
+
+builder.Services.AddMediatR(cfg =>
+{
+    cfg.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+});
+
 builder.Services.AddScoped<IAstronautDutyDomainService, AstronautDutyDomainService>();
+
+// shows logs in Log Stream in Azure Portal
+builder.Logging.AddConsole();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<StargateContext>();
+    db.Database.Migrate();
 }
 
 app.UseHttpsRedirection();
